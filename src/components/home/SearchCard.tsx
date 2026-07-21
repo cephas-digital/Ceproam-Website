631c53eadae906b8f2b84f8f5000206a2062b7b2
feranmi <feranmij10@gmail.com>
2026-07-20 20:09:28 -1200

    search-card

--- src/components/home/SearchCard.tsx
+++ src/components/home/SearchCard.tsx
@@ -6,22 +6,22 @@ import { FaMountain } from "react-icons/fa";
 
 const SearchCard = () => {
   return (
-    <div className="mx-auto w-full rounded-[1.5rem] border border-white/30 bg-white/60 p-4 shadow-xl backdrop-blur-lg sm:p-6">
-      <div className="flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
+    <div className="mx-auto w-full  rounded-[1.5rem] border border-white/30 bg-white/60 p-4 shadow-xl backdrop-blur-lg sm:p-6">
+      <div className="flex flex-row md:overflow-hidden overflow-x-scroll gap-3 sm:flex-row sm:items-center sm:justify-between">
         <Link
           to="/properties"
-          className="flex w-full items-center justify-center gap-2 rounded-full bg-[#002B66] px-5 py-3 text-sm font-medium text-white sm:w-auto sm:min-w-[148px] sm:text-base"
+          className="flex md:w-full min-w-[200px]   items-center justify-center gap-2 rounded-full bg-[#002B66] px-5 py-3 text-sm font-medium text-white sm:w-auto sm:min-w-[148px] sm:text-base"
         >
           <IoHomeOutline />
           Property
         </Link>
 
-        <button className="flex w-full items-center justify-center gap-2 rounded-full bg-white px-5 py-3 text-sm font-medium text-gray-700 sm:w-auto sm:min-w-[148px] sm:text-base">
+        <button className="flex md:w-full min-w-[200px]  items-center justify-center gap-2 rounded-full bg-white px-5 py-3 text-sm font-medium text-gray-700 sm:w-auto sm:min-w-[148px] sm:text-base">
           <GiFarmTractor />
           Agri-Projects
         </button>
 
-        <button className="flex w-full items-center justify-center gap-2 rounded-full bg-white px-5 py-3 text-sm font-medium text-gray-700 sm:w-auto sm:min-w-[148px] sm:text-base">
+        <button className="flex md:w-full min-w-[200px]  items-center justify-center gap-2 rounded-full bg-white px-5 py-3 text-sm font-medium text-gray-700 sm:w-auto sm:min-w-[148px] sm:text-base">
           <FaMountain />
           Land Banking
         </button>
@@ -33,7 +33,7 @@ const SearchCard = () => {
           <p className="font-semibold text-[#1A1B1F]">Nairobi & Environs</p>
         </div>
 
-        <button className="flex w-full items-center justify-center rounded-full bg-white p-3 text-black shadow-md sm:w-auto">
+        <button className="md:flex hidden w-full items-center justify-center rounded-full bg-white p-3 text-black shadow-md sm:w-auto">
           <HiOutlineAdjustmentsHorizontal size={22} />
         </button>
       </div>
